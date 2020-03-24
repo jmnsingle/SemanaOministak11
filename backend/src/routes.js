fef71d640de63c6bff5d902470a995d2c32f1f1e
Juliano <jmnogueira2011@gmail.com>
2020-03-24 19:51:00 -0300

    Backend Created.

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.store )
+
+routes.post('/ongs', OngController.store);
+routes.get('/ongs', OngController.index);
+
+routes.post('/incidents', IncidentController.store);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+routes.get('/profiles', ProfileController.index);
+
+
+module.exports = routes;
